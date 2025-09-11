5f9fcf3093a72355bd0e3b3210dcbd0cc8bfb907
Pedro <pedroardo10@gmail.com>
2025-09-11 12:35:03 +0700

    making easir to read the output

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// function pattern(n) {
-//   for (let i = 1; i <= n; i++) {
-//     console.log(" ".repeat(n - i) + "*".repeat(2 * i - 1));
-//   }
+function pattern(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(" ".repeat(n - i) + "*".repeat(2 * i - 1));
+  }
 
-//   // lower inverted pyramid
-//   for (let i = n - 1; i >= 1; i--) {
-//     console.log(" ".repeat(n - i) + "*".repeat(2 * i - 1));
-//   }
-// }
-// pattern(4);
+  // lower inverted pyramid
+  for (let i = n - 1; i >= 1; i--) {
+    console.log(" ".repeat(n - i) + "*".repeat(2 * i - 1));
+  }
+}
+console.log("1. Pattern: ");
+pattern(4);
 
 function anagram(firstWord, secondWord) {
   const first = firstWord.split("").sort().join("");
@@ -24,4 +25,4 @@ function anagram(firstWord, secondWord) {
 
   return first === second; // untuk cek apakah 2 kata diatas ini memiliki huruf yang sama atau tidak
 }
-console.log(anagram("listen", "silent"));
+console.log("2. Anagram: ", anagram("listen", "silent"));
