2c04c897172280236a8a2d2220309e3e23ff9a71
Pedro <pedroardo10@gmail.com>
2025-09-11 12:34:01 +0700

    learn anagram

--- index.js
+++ index.js
@@ -6,14 +6,22 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-function pattern(n) {
-  for (let i = 1; i <= n; i++) {
-    console.log(" ".repeat(n - i) + "*".repeat(2 * i - 1));
-  }
+// function pattern(n) {
+//   for (let i = 1; i <= n; i++) {
+//     console.log(" ".repeat(n - i) + "*".repeat(2 * i - 1));
+//   }
 
-  // lower inverted pyramid
-  for (let i = n - 1; i >= 1; i--) {
-    console.log(" ".repeat(n - i) + "*".repeat(2 * i - 1));
-  }
+//   // lower inverted pyramid
+//   for (let i = n - 1; i >= 1; i--) {
+//     console.log(" ".repeat(n - i) + "*".repeat(2 * i - 1));
+//   }
+// }
+// pattern(4);
+
+function anagram(firstWord, secondWord) {
+  const first = firstWord.split("").sort().join("");
+  const second = secondWord.split("").sort().join("");
+
+  return first === second; // untuk cek apakah 2 kata diatas ini memiliki huruf yang sama atau tidak
 }
-pattern(4);
+console.log(anagram("listen", "silent"));
