fb505728a5cd9daf944c82de0944951f6bbc35a0
Pedro <pedroardo10@gmail.com>
2025-10-31 17:46:56 +0700

    add twoSum func

--- index.js
+++ index.js
@@ -58,3 +58,14 @@ function gradingStudent(num) {
   }
   return result;
 }
+
+function twoSum(nums, target) {
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+console.log(twoSum([2, 7, 11, 15], 9));
