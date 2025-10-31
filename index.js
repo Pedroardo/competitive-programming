dcea92f3c6a65c597fd3e9ea653c8411e52b086f
Pedro <pedroardo10@gmail.com>
2025-10-31 16:29:53 +0700

    add gradingStudent func

--- index.js
+++ index.js
@@ -26,3 +26,35 @@ function anagram(firstWord, secondWord) {
   return first === second; // untuk cek apakah 2 kata diatas ini memiliki huruf yang sama atau tidak
 }
 console.log("2. Anagram: ", anagram("listen", "silent"));
+
+function reverseText(text) {
+  return text.split("").reverse().join("");
+}
+console.log("3.Reverse text: ", reverseText("hello"));
+
+function anagrams(k1, k2) {
+  const test = k1.split("").sort().join("");
+  const test2 = k2.split("").sort().join("");
+  return test === test2;
+}
+
+console.log(anagrams("listen", "silent"));
+
+function gradingStudent(num) {
+  const result = [];
+
+  for (let grade of num) {
+    if (grade < 38) {
+      result.push(grade);
+    } else {
+      const nextMultiple = Math.ceil(grade / 5) * 5;
+
+      if (grade - nextMultiple < 3) {
+        result.push(nextMultiple);
+      } else {
+        result.push(grade);
+      }
+    }
+  }
+  return result;
+}
